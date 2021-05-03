dabfd6dc49c9cb7ad496a188a56d1429dbe4e8cc
Artiom P <apoluyanovich@gmail.com>
2021-05-03 15:35:40 +0300

    update Rate model

--- MoneyConverter/Managers/NetworkManager.swift
+++ MoneyConverter/Managers/NetworkManager.swift
@@ -17,15 +17,14 @@ class NetworkManager {
     private init() {}
     static let shared = NetworkManager()
     
-    let key = "667fa215140eb6a05a16c31a"
+    private let key = "667fa215140eb6a05a16c31a"
     
     func getRate(code: Code, complition: @escaping ((Rate?) -> ())) {
         guard let url = URL(string: "https://v6.exchangerate-api.com/v6/\(key)/latest/\(code.rawValue)") else { return }
         URLSession.shared.dataTask(with: url) { (data, _, error) in
             guard let data = data else { return }
-            var decoderRate : Rate?
             do {
-                decoderRate = try JSONDecoder().decode(Rate.self, from: data)
+                let decoderRate = try JSONDecoder().decode(Rate.self, from: data)
                 DispatchQueue.main.async {
                     complition(decoderRate)
                 }
--- MoneyConverter/Models/Rate.swift
+++ MoneyConverter/Models/Rate.swift
@@ -7,12 +7,26 @@
 
 struct Rate: Codable {
     let baseCode: String?
-    let conversionRates: [String: Double]?
+    let conversionRates: ConversionRates?
 
     enum CodingKeys: String, CodingKey {
         case baseCode = "base_code"
         case conversionRates = "conversion_rates"
     }
+    
+    struct ConversionRates: Codable{
+        let rus: Double?
+        let eur: Double?
+        let usd: Double?
+        
+        enum CodingKeys: String, CodingKey {
+            case rus = "RUS"
+            case eur = "EUR"
+            case usd = "USD"
+        }
+    }
+
 }
+
 
 
